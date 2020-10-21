a33be870358e97605d934294f563cf9adaf963bd
C1316363 David Jeremias Souza do Carmo <c1316363@interno.bb.com.br>
2020-10-21 10:44:37 -0300

    first commit

---
+++ src/main/java/com/u2d/projeto/CheckmatApplication.java
@@ -0,0 +1,12 @@
+package com.u2d.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckmatApplication.class, args);
+	}
+}
